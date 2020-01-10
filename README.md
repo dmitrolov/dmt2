79a747167daced561f3f2cbbb680ce5bb6d4bb32
dmitrolov <dmytro_frolov1@epam.com>
2020-01-10 20:30:56 +0200

    GitHub home page add Build

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## This is Dungeon Master Tools
-#####Source [GitHub](https://github.com/dmitrolov/dmt2)
-#####Hosting [GitHub Pages](https://dmitrolov.github.io/dmt2/)
+###Source [GitHub](https://github.com/dmitrolov/dmt2)
+###Hosting [GitHub Pages](https://dmitrolov.github.io/dmt2/)
 
 
 ## Readme.md edit examples
---
+++ docs/build/precache-manifest.7ee9bfe420183fbd59aac61fd17fae68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81f73cb860fcdfa9cb15a517efc4089",
+    "url": "/dmt2/index.html"
+  },
+  {
+    "revision": "87d63080c7ee224e4a9e",
+    "url": "/dmt2/static/css/main.4367f119.chunk.css"
+  },
+  {
+    "revision": "ab4e47cd543c755d6ed8",
+    "url": "/dmt2/static/js/2.fc02ac24.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dmt2/static/js/2.fc02ac24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87d63080c7ee224e4a9e",
+    "url": "/dmt2/static/js/main.de063e06.chunk.js"
+  },
+  {
+    "revision": "4b607bc4080bbb0623a5",
+    "url": "/dmt2/static/js/runtime-main.77f6908f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/dmt2/static/media/logo.ee7cd8ed.svg"
+  }
+]);
