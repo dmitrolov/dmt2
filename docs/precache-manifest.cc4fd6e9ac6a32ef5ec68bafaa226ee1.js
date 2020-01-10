1927f5bf7d0c6a005c11738401d041b4dbba9d8b
dmitrolov <dmytro_frolov1@epam.com>
2020-01-10 18:49:26 +0200

    GH pages folder add

---
+++ docs/precache-manifest.cc4fd6e9ac6a32ef5ec68bafaa226ee1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c1bad148e847051897bf4eff017f6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b17eb4c048bb961e931f",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "ab4e47cd543c755d6ed8",
+    "url": "/static/js/2.fc02ac24.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fc02ac24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b17eb4c048bb961e931f",
+    "url": "/static/js/main.bf422369.chunk.js"
+  },
+  {
+    "revision": "26cff7493fadcdcef06e",
+    "url": "/static/js/runtime-main.d8a73986.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
