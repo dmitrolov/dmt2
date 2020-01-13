6589173af985f3103213cda9ca0bd904534627bc
dmitrolov <dmytro_frolov1@epam.com>
2020-01-13 20:38:58 +0200

    Ant UI added

---
+++ docs/build/precache-manifest.8e63d5e09b70391109b46cf7cdfccda5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07480d4923189a4fc04321561e83c1b",
+    "url": "/dmt2/index.html"
+  },
+  {
+    "revision": "4186d98c2b4e749479ca",
+    "url": "/dmt2/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "cd87bf37306853ed882e",
+    "url": "/dmt2/static/css/main.b811a0b8.chunk.css"
+  },
+  {
+    "revision": "4186d98c2b4e749479ca",
+    "url": "/dmt2/static/js/2.2816aad6.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/dmt2/static/js/2.2816aad6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd87bf37306853ed882e",
+    "url": "/dmt2/static/js/main.17d201c0.chunk.js"
+  },
+  {
+    "revision": "4b607bc4080bbb0623a5",
+    "url": "/dmt2/static/js/runtime-main.77f6908f.js"
+  }
+]);
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,90 @@
-import React from 'react';
+import { Breadcrumb, Button, Icon, Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
+import React, { useState } from 'react';
 import './App.scss';
-import logo from '../img/logo.svg';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 const App: React.FC = () => {
+  const [status, isMenuCollapsed] = useState(true);
+
+  const onMenuClick = () => {
+    isMenuCollapsed(!status);
+  };
+
   return (
     <>
-      <h1>Oh shit, here we go again</h1>
-      <img src={ logo } alt="" />
+      <Layout style={ { minHeight: '100vh' } }>
+        <Sider collapsible collapsed={ status } onCollapse={ onMenuClick } width={320}>
+          <div style={ { width: 320 } }>
+            <Button type="primary" onClick={ onMenuClick } style={ { marginBottom: 16 } }>
+              <Icon type={ status ? 'menu-unfold' : 'menu-fold' } />
+            </Button>
+            <Menu
+              defaultSelectedKeys={ ['1'] }
+              // defaultOpenKeys={['sub1']}
+              mode="inline"
+              theme="dark"
+              inlineCollapsed={ status }
+            >
+              <Menu.Item key="1">
+                <Icon type="pie-chart" />
+                <span>Option 1</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="desktop" />
+                <span>Option 2</span>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="inbox" />
+                <span>Option 3</span>
+              </Menu.Item>
+              <SubMenu
+                key="sub1"
+                title={
+                  <span>
+                <Icon type="mail" />
+                <span>Navigation One</span>
+              </span>
+                }
+              >
+                <Menu.Item key="5">Option 5</Menu.Item>
+                <Menu.Item key="6">Option 6</Menu.Item>
+                <Menu.Item key="7">Option 7</Menu.Item>
+                <Menu.Item key="8">Option 8</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub2"
+                title={
+                  <span>
+                <Icon type="appstore" />
+                <span>Navigation Two</span>
+              </span>
+                }
+              >
+                <Menu.Item key="9">Option 9</Menu.Item>
+                <Menu.Item key="10">Option 10</Menu.Item>
+                <SubMenu key="sub3" title="Submenu">
+                  <Menu.Item key="11">Option 11</Menu.Item>
+                  <Menu.Item key="12">Option 12</Menu.Item>
+                </SubMenu>
+              </SubMenu>
+            </Menu>
+          </div>
+        </Sider>
+        <Layout>
+          <Header style={ { background: '#fff', padding: 0 } } />
+          <Content style={ { margin: '0 16px' } }>
+            <Breadcrumb style={ { margin: '16px 0' } }>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={ { padding: 24, background: '#fff', minHeight: 360 } }>Bill is a cat.</div>
+          </Content>
+          <Footer style={ { textAlign: 'center' } }>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
     </>
   );
 };
