9235d0b3cc1b1ebd736104486947e61e2eb29036
dmitrolov <dmytro_frolov1@epam.com>
2020-01-31 17:45:40 +0200

    Dev static page container

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dmt2/precache-manifest.e3b20e376377065320b4ca29ea7333a4.js"
+  "/dmt2/precache-manifest.73c7e1c8487f860f892c1401055b14f4.js"
 );
 
 self.addEventListener('message', (event) => {
