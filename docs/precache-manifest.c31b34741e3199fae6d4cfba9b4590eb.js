b61573eae77cb38e2591930779009000753146af
dmitrolov <dmitrolov@gmail.com>
2020-07-07 03:00:15 +0300

    another build

---
+++ docs/precache-manifest.c31b34741e3199fae6d4cfba9b4590eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21125dbc6e71547e3102072cd5b5d6b4",
+    "url": "/dmt2/index.html"
+  },
+  {
+    "revision": "489c1ef03b38583798b5",
+    "url": "/dmt2/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "19f3bff6221b9e8f27f5",
+    "url": "/dmt2/static/css/main.326f345a.chunk.css"
+  },
+  {
+    "revision": "489c1ef03b38583798b5",
+    "url": "/dmt2/static/js/2.5ccd875d.chunk.js"
+  },
+  {
+    "revision": "45292ea1379052fb670d78e4042d895e",
+    "url": "/dmt2/static/js/2.5ccd875d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19f3bff6221b9e8f27f5",
+    "url": "/dmt2/static/js/main.ae7d5794.chunk.js"
+  },
+  {
+    "revision": "4b607bc4080bbb0623a5",
+    "url": "/dmt2/static/js/runtime-main.77f6908f.js"
+  }
+]);
